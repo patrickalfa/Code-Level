06c3715985a3612730f9c3de361b92daa4ebf83a
Patrick Clemer Alfarth <patrick.c.alfa@gmail.com>
2018-10-17 21:48:40 -0300

    Add CRT post-processing effects

---
+++ Assets/Plugins/Realistic Glitches Lite/Assets/Scripts/ShaderEffect_Distortion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShaderEffect_Distortion : MonoBehaviour
+{
+    public float intensity = 3;
+    public float valueX = 0.5f;
+    public Texture displacement;
+    private Material material;
+
+    // Creates a private material used to the effect
+    void Awake()
+    {
+        material = new Material(Shader.Find("Hidden/Distortion"));
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        material.SetFloat("_Intensity", intensity * 0.01f);
+        material.SetFloat("_ValueX", valueX);
+        material.SetTexture("_Texture", displacement);
+
+        Graphics.Blit(source, destination, material);
+    }
+}
